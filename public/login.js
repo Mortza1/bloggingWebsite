a826edb2a4d0cf1c4505332cb2d845176edf8e2d
murtazaHw26 <145095363+murtazaHw26@users.noreply.github.com>
2024-01-05 18:27:02 +0400

    some css correction

--- public/login.js
+++ public/login.js
@@ -32,3 +32,56 @@ document.getElementById('loginButton').addEventListener('click', function() {
         console.error('There was a problem with the fetch operation:', error);
     });
 });
+
+
+let loginSelector = document.getElementById("loginSelector");
+let signupSelector = document.getElementById("signupSelector");
+let loginBox = document.querySelector(".login-box");
+let signUpBox = document.querySelector(".signup-box");
+
+loginSelector.addEventListener("click", function(){
+    signUpBox.classList.remove("active");
+    loginBox.classList.add("active");
+});
+
+signupSelector.addEventListener("click", function(){
+    loginBox.classList.remove("active");
+    signUpBox.classList.add("active");
+});
+
+
+document.getElementById("registerButton").addEventListener('click', function() {
+    const username = document.getElementById("usernameInput").value;
+    const email = document.getElementById("reg-emailInput").value;
+    const password = document.getElementById("reg-passwordInput").value;
+    const confirmPassword = document.getElementById("reg-passwordInputConfirm").value;
+
+    if(password === confirmPassword){
+        const data = {
+            username: username,
+            email: email,
+            password: password
+        };
+
+        fetch('/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (!response.ok){
+                throw new Error('Network response was not ok');
+            }
+
+            return response.json();
+        })
+        .then(data => {
+            console.log('Response from server:', data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        })
+    }
+})
